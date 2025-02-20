c55bcbce05361dbc03590df2bd9e819c26d74667
Ansh Shah <ansh_shah@berkeley.edu>
2025-02-19 22:38:23 -0800

    Implemented abs, relu, and argmax (up to task 3), TODO: task 4 and task 5

--- src/argmax.s
+++ src/argmax.s
@@ -16,15 +16,36 @@
 # =================================================================
 argmax:
     # Prologue
-
+    add t0 a0 zero  # initialize the indexer (t0)
+    blt zero a1 loop_start # So that length >= 1 (a1)
+    addi a0 zero 36
+    j exit
 
 loop_start:
-
-
+    # Take in a value from the array
+    lw t1 0(t0) # This will be the max (t1)
+    add t3 zero zero # This will be the max index
+    
 loop_continue:
-
+    # Decrease the count
+    addi a1 a1 -1
+    # if we're done looking at stuff
+    beq zero a1 loop_end
+    # otherwise, increase the indexer to the next word
+    addi t0 t0 4
+    # let's take a look at the value there
+    lw t2 0(t0) # temporary value to compare to (t2)
+    # if max >= temp, continue
+    bge t1 t2 loop_continue
+    # otherwise, reassign the max
+    add t1 t2 zero
+    # Update the index
+    sub t3 t0 a0  # Find the distance (multiples of 4) from the start
+    srli t3 t3 2  # Divide by 4 to get the index
+    # Continue
+    j loop_continue
 
 loop_end:
     # Epilogue
-
+    add a0 zero t3
     jr ra
--- src/relu.s
+++ src/relu.s
@@ -14,25 +14,29 @@
 # ==============================================================================
 relu:
     # Prologue
-
+    add t0 a0 zero  # t0 will index the list
+    blt zero a1 loop_start
+    li a0 36
+    j exit
 
 loop_start:
-
-
-
-
-
-
-
+    # Read the first index
+    lw t1 0(t0)
+    # Check if it's less than 0
+    bge t1 zero loop_continue
+    # If it is less than 0, we need to store a 0 at the location
+    sw zero 0(t0)
 
 loop_continue:
-
-
+    # Decrease iterations
+    addi a1 a1 -1
+    # Let's check if we're done
+    beq zero a1 loop_end
+    # Increment the index pointer by 4 (sizeof(int))
+    addi t0 t0 4
+    # Loop again
+    j loop_start
 
 loop_end:
-
-
     # Epilogue
-
-
     jr ra
